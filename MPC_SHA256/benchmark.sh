9b10eeb0d7df06d6213ae7be3164d245afcd0ce0
Nina <au674305@uni.au.dk>
2023-05-22 13:02:16 +0200

    refactor: script and code

--- MPC_SHA256/benchmark.sh
+++ MPC_SHA256/benchmark.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-echo "Enter number of threads."
-read THREADS
-
 gcc MPC_SHA256.c -o mpc_proof -L/usr/lib64 -lcrypto -fopenmp -w
 gcc MPC_SHA256_VERIFIER.c -o mpc_verify -L/usr/lib64 -lcrypto -fopenmp -w
 
 for n in {1..1000};
 do
     echo "Running benchmark $n"
-    echo "hello world" | OMP_NUM_THREADS=$THREADS ./mpc_proof >> proof_times.txt
-    OMP_NUM_THREADS=$THREADS ./mpc_verify >> verify_times.txt
+    echo "hello world" | OMP_NUM_THREADS=$1 ./mpc_proof >> proof_times.txt
+    OMP_NUM_THREADS=$1 ./mpc_verify >> verify_times.txt
 
     echo "" >> proof_times.txt
     echo "" >> proof_times.txt
--- bench_linux.sh
+++ bench_linux.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-cd MPC_SHA256
-./benchmark.sh
-cd ..
-python3 python/parse_times.py -p MPC_SHA256/proof_times.txt -v MPC_SHA256/verify_times.txt
-rm MPC_SHA256/proof_times.txt MPC_SHA256/verify_times.txt
+
+echo "Enter max number of threads."
+read THREADS
+
+START=1
+
+for ((n=$START; n<$THREADS; n++))
+do
+    echo "Running for $n threads"
+    cd MPC_SHA256
+    ./benchmark.sh $n
+    cd ..
+    python3 python/parse_times.py -p MPC_SHA256/proof_times.txt -v MPC_SHA256/verify_times.txt -o os-name-threads-$n.csv
+    rm MPC_SHA256/proof_times.txt MPC_SHA256/verify_times.txt
+done
