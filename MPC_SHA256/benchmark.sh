5e0a7220bd2f6567cde33ca03c493462e425c85a
Nina <au674305@uni.au.dk>
2023-05-22 12:40:08 +0200

    refactor: benchmarking code

--- MPC_SHA256/benchmark.sh
+++ MPC_SHA256/benchmark.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Enter number of threads."
-read threads
+read THREADS
 
-gcc MPC_SHA256.c -o mpc_proof -L/usr/lib64 -lcrypto -lomp -w
-gcc MPC_SHA256_VERIFIER.c -o mpc_verify -L/usr/lib64 -lcrypto -lomp -w
+gcc MPC_SHA256.c -o mpc_proof -L/usr/lib64 -lcrypto -fopenmp -w
+gcc MPC_SHA256_VERIFIER.c -o mpc_verify -L/usr/lib64 -lcrypto -fopenmp -w
 
 for n in {1..1000};
 do
     echo "Running benchmark $n"
-    echo "hello world" | ./mpc_proof $threads >> proof_times.txt
-    ./mpc_verify $threads >> verify_times.txt
+    echo "hello world" | OMP_NUM_THREADS=$THREADS ./mpc_proof >> proof_times.txt
+    OMP_NUM_THREADS=$THREADS ./mpc_verify >> verify_times.txt
 
     echo "" >> proof_times.txt
     echo "" >> proof_times.txt
