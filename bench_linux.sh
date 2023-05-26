9aae6f6dd2305353eb0280a6be4e192dd42300f0
Nina <au674305@uni.au.dk>
2023-05-26 08:10:45 +0200

    refactor: script to save parsed files as different name

--- bench_linux.sh
+++ bench_linux.sh
@@ -9,6 +9,6 @@ do
     cd MPC_SHA256
     ./benchmark.sh $n
     cd ..
-    python3 python/parse_times.py -p MPC_SHA256/proof_times.txt -v MPC_SHA256/verify_times.txt -o os-name-threads-$n.csv
+    python3 python/parse_times.py -p MPC_SHA256/proof_times.txt -v MPC_SHA256/verify_times.txt -o threads-$n.csv
     rm MPC_SHA256/proof_times.txt MPC_SHA256/verify_times.txt
 done
